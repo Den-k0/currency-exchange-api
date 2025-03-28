f082122d42ac48c47a420a12a847de042eb37522
Den_k0 <denys.kotovskiy@gmail.com>
2025-03-28 11:29:53 +0200

    implement exchange rate api util & update env.sample

---
+++ currency/utils.py
@@ -0,0 +1,39 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("EXCHANGE_RATE_API_KEY")
+EXCHANGE_RATE_API_URL = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/USD"
+
+
+def get_exchange_rate(currency_code: str) -> float:
+    """
+    Fetch the exchange rate for a given currency code.
+
+    Args:
+        currency_code (str): The currency code for which to fetch the exchange rate.
+
+    Returns:
+        float: The exchange rate for the specified currency code, or None if the currency code is not found.
+
+    Raises:
+        ValueError: If the ExchangeRate API key is not configured,
+                    if there is an error fetching the exchange rates,
+                    or if the request times out.
+    """
+    if not api_key:
+        raise ValueError("ExchangeRate API key not configured")
+
+    try:
+        response = requests.get(EXCHANGE_RATE_API_URL, timeout=5)
+        data = response.json()
+
+        if response.status_code != 200 or "conversion_rates" not in data:
+            raise ValueError("Error fetching exchange rates")
+
+        return data["conversion_rates"].get(currency_code.upper(), None)
+    except requests.Timeout:
+        raise ValueError("Exchange rate API timeout")
